8410d3ed61f94d11502c612ac0a28b62524ce991
PichlerMathias <s2210307788@fhooe.at>
2026-04-01 23:20:23 +0200

    Add validation for partitions inside IN

--- src/main/java/marchvalidation/dom/annotators/ArchRuleAnnotator.java
+++ src/main/java/marchvalidation/dom/annotators/ArchRuleAnnotator.java
@@ -4,82 +4,113 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.util.xml.DomManager;
 import marchvalidation.dom.MarchConfigRoot;
 import marchvalidation.dom.util.MarchConfigUtil;
+import marchvalidation.psi.ArchComparisonWrap;
 import marchvalidation.psi.ArchPartitionExpr;
 import marchvalidation.psi.ArchRuleTypes;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArchRuleAnnotator implements Annotator {
 
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-        if (!(element instanceof ArchPartitionExpr expr)) {
+        if (element instanceof ArchPartitionExpr expr) {
+            validatePartitionExpr(expr, holder);
+        }
+        else if (element instanceof ArchComparisonWrap wrap) {
+            validateComparisonWrap(wrap, holder);
+        }
+    }
+
+    private void validatePartitionExpr(ArchPartitionExpr expr, AnnotationHolder holder) {
+        final var literals = getLiteralsFromElement(expr);
+        if (literals.isEmpty()) {
             return;
         }
 
-        final var literalNodes = getLiterals(expr);
-        if (literalNodes.isEmpty()) {
-            return;
-        }
-
-        final var file = element.getContainingFile();
-        final var originalFile = file.getContext() != null ? file.getContext().getContainingFile() : file;
-
-        if (!(originalFile instanceof XmlFile xmlFile)) {
-            return;
-        }
-
-        final var root = DomManager.getDomManager(xmlFile.getProject())
-                .getFileElement(xmlFile, MarchConfigRoot.class)
-                .getRootElement();
-        final var cache = MarchConfigUtil.getCache(root);
-
-        final var dimNode = literalNodes.get(0);
-        final var dimName = dimNode.getText();
-
+        final var dimName = literals.get(0).getText();
         if (isKeyword(dimName)) {
             return;
         }
 
-        final var dim = cache.getDimensions().get(dimName);
+        final var dim = resolveDimension(expr, dimName);
         if (dim == null) {
-            holder.newAnnotation(HighlightSeverity.ERROR, "Dimension '" + dimName + "' is not defined")
-                    .range(dimNode)
-                    .create();
+            markError(holder, literals.get(0), "Dimension '" + dimName + "' is not defined");
             return;
         }
 
-        if (literalNodes.size() > 1) {
-            final var partNode = literalNodes.get(1);
-            final var partName = partNode.getText();
+        // If it's 'layer.dto', literals.get(1) is 'dto'
+        if (literals.size() > 1) {
+            validatePartition(dim, literals.get(1), holder, dimName);
+        }
+    }
 
-            final var exists = dim.getPartitionsElement().getPartitions().stream()
-                    .anyMatch(p -> partName.equals(p.getName().getStringValue()));
+    private void validateComparisonWrap(ArchComparisonWrap wrap, AnnotationHolder holder) {
+        final var dimNode = wrap.getLiteral();
+        if (dimNode == null) {
+            return;
+        }
 
-            if (!exists) {
-                holder.newAnnotation(HighlightSeverity.ERROR, "Partition '" + partName + "' is not defined for dimension '" + dimName + "'")
-                        .range(partNode)
-                        .create();
+        final var dimName = dimNode.getText();
+        final var dim = resolveDimension(wrap, dimName);
+
+        if (dim == null) {
+            markError(holder, dimNode, "Dimension '" + dimName + "' is not defined");
+            return;
+        }
+
+        final var options = wrap.getInOptions();
+        if (options != null) {
+            final var partitions = getLiteralsFromElement(options);
+            for (PsiElement partNode : partitions) {
+                validatePartition(dim, partNode, holder, dimName);
             }
         }
     }
 
-    private List<PsiElement> getLiterals(ArchPartitionExpr expr) {
-        final var results = new ArrayList<PsiElement>();
-        var child = expr.getFirstChild();
-        while (child != null) {
-            if (child.getNode().getElementType() == ArchRuleTypes.LITERAL) {
-                results.add(child);
-            }
-            child = child.getNextSibling();
+    private void validatePartition(marchvalidation.dom.Dimension dim, PsiElement partNode, AnnotationHolder holder, String dimName) {
+        final var partName = partNode.getText();
+        final var exists = dim.getPartitionsElement().getPartitions().stream()
+                .anyMatch(p -> partName.equals(p.getName().getStringValue()));
+
+        if (!exists) {
+            markError(holder, partNode, "Partition '" + partName + "' is not defined for dimension '" + dimName + "'");
         }
-        return results;
+    }
+
+    private marchvalidation.dom.Dimension resolveDimension(PsiElement element, String dimName) {
+        final var file = element.getContainingFile();
+        final var originalFile = file.getContext() != null ? file.getContext().getContainingFile() : file;
+        if (!(originalFile instanceof XmlFile xmlFile)) {
+            return null;
+        }
+
+        final var root = DomManager.getDomManager(xmlFile.getProject())
+                .getFileElement(xmlFile, MarchConfigRoot.class);
+        if (root == null) {
+            return null;
+        }
+
+        final var cache = MarchConfigUtil.getCache(root.getRootElement());
+        return cache.getDimensions().get(dimName);
+    }
+
+    private List<PsiElement> getLiteralsFromElement(PsiElement element) {
+        return PsiTreeUtil.findChildrenOfType(element, PsiElement.class).stream()
+                .filter(e -> e.getNode().getElementType() == ArchRuleTypes.LITERAL)
+                .toList();
+    }
+
+    private void markError(AnnotationHolder holder, PsiElement node, String message) {
+        holder.newAnnotation(HighlightSeverity.ERROR, message)
+                .range(node)
+                .create();
     }
 
     private boolean isKeyword(String name) {
